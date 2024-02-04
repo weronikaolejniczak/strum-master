d9f8e4f7a1aff08040ec8db70e1640d46c5bee19
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-02-04 17:05:35 +0100

    chore(ui): configure TailwindCSS for Storybook

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -17,6 +17,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
     getAbsolutePath('@storybook/addon-a11y'),
+    getAbsolutePath('@storybook/addon-styling-webpack'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-webpack5'),
